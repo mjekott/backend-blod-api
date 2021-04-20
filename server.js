09626b202be5b054cecdcd8cfc07fdd54ecd1a6a
mjekott <ekottmfon@yahoo.com>
2021-04-20 12:58:27 +0100

    configured cors for poduction adn set up database connection with mongoose

--- server.js
+++ server.js
@@ -3,17 +3,32 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 //app
 const app = express();
+
+//database connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connected'));
 
 //middlewares
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(cors());
+
+//applying cors
+if (process.env.NODE_ENV === 'production') {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
 
 //routes
 app.get('/api', (req, res, next) => {
